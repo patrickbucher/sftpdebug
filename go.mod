651e7ef02787db611174c21d94f3a800165b2dff
Patrick Bucher <patrick.bucher@peax.ch>
2019-10-04 21:02:02 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.13
+
+require (
+	github.com/pkg/sftp v1.10.1
+	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	hostnameEnv = "SFTP_HOSTNAME"
+	portEnv     = "SFTP_PORT"
+	usernameEnv = "SFTP_USERNAME"
+	passwordEnv = "SFTP_PASSWORD"
+)
+
+func main() {
+	hostname := os.Getenv(hostnameEnv)
+	port, err := strconv.Atoi(os.Getenv(portEnv))
+	username := os.Getenv(usernameEnv)
+	password := os.Getenv(passwordEnv)
+	if hostname == "" || err != nil || username == "" || password == "" {
+		fmt.Fprintln(os.Stderr, "missing connection parameters")
+		os.Exit(1)
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+	}
+	socket := fmt.Sprintf("%s:%d", hostname, port)
+	sshConn, err := ssh.Dial("tcp", socket, sshConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "establish ssh connection: %w", err)
+		os.Exit(1)
+	}
+	sftp, err := sftp.NewClient(sshConn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "establish sftp connection: %w", err)
+		os.Exit(1)
+	}
+	fmt.Println(sftp)
+}
